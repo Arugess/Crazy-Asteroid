6713a336a6c62e9ed193724d74973fa67ef0aa8e
Arugess <georgemeharris@gmail.com>
2019-04-13 18:05:38 -0700

    Update TimerText.cs

--- TimerText.cs
+++ TimerText.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//This makes the text for the timer to count downward.//
+
 public class TimerText : MonoBehaviour
 {
 
