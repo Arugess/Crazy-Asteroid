0abb69d697f745073dbd93c74d44c54d968587c7
Arugess <georgemeharris@gmail.com>
2019-04-13 17:49:32 -0700

    Add files via upload

---
+++ LifeHUD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeHUD : MonoBehaviour
+{
+    public static int liveLeft; 
+    Text lives;
+
+	void Start ()
+    {
+        lives = GetComponent<Text>();
+    }
+	
+	void Update ()
+    {
+        liveLeft = PlayerStuff.playerHP;
+
+        lives.text = "x " + liveLeft;
+	}
+}
