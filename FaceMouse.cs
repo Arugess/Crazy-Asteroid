3748c5ba6b5f468968b38bcfafad4bd9e01aeb84
Arugess <georgemeharris@gmail.com>
2019-04-13 17:42:10 -0700

    Add files via upload

---
+++ FaceMouse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceMouse : MonoBehaviour
+{
+
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        faceMouse();
+
+
+	}
+
+    void faceMouse()
+    {
+        //Finds mouse position in level
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        //Points the arrow towards the mouse
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+
+    }
+
+}
