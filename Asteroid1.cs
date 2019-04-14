bd8fe73a5ecf4af049f104d12086722442225edb
Arugess <georgemeharris@gmail.com>
2019-04-13 17:27:08 -0700

    Add files via upload
    This script spawns an asteroid that moves to the players location at the time it was spawned. It is not able to continuously track the player.

---
+++ Asteroid1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid1 : MonoBehaviour
+{
+    //The speed, and rigidbody of the asteroid
+    float asteroidSpeed = 4.0f;
+    Transform target;
+
+    Animator asteroidAnim;
+
+    Vector2 startDir;
+
+    void Start ()
+    {
+        //Gets the animator
+        asteroidAnim = GetComponent<Animator>();
+     
+        //target finds the players initial coordinates, startDir prevent the asteroid from contiously tracking the player
+        target = GameObject.FindWithTag("Player").transform;
+        startDir = target.position;
+    }
+	
+	void Update ()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float moveAsteroid = asteroidSpeed * Time.deltaTime;
+
+
+        transform.position = Vector2.MoveTowards(transform.position, startDir, moveAsteroid);
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Missile")
+        {
+            asteroidAnim.SetTrigger("kill");
+            StartCoroutine("DestroyAsteroid");
+        }
+
+
+    }
+
+    IEnumerator DestroyAsteroid()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Destroy(gameObject);
+    }
+}
