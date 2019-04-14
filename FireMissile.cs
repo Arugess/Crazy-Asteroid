28775b344e7fccb4544b3518e8f9f161f243b18b
Arugess <georgemeharris@gmail.com>
2019-04-13 17:47:00 -0700

    Add files via upload

---
+++ FireMissile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMissile : MonoBehaviour
+{
+    public GameObject missile1;
+    public Transform earthArrow;
+    public static bool canFire;
+
+
+	void Start ()
+    {
+        canFire = true;
+	}
+	
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Mouse0) && canFire)
+        {
+ 
+            GameObject missile1Instance;
+            missile1Instance = Instantiate(missile1, earthArrow.position, earthArrow.rotation);
+            missile1Instance.GetComponent<Rigidbody2D>().AddForce(transform.up * 600);
+            canFire = false;
+            StartCoroutine("Reload");
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(3.0f);
+        canFire = true;
+    }
+
+
+}
